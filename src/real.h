ad70167c976784d11a28084b2baed8b2a0329008
Yatao Li <yatli@microsoft.com>
2020-05-16 22:59:58 +0800

    support different numeric systems

--- src/real.h
+++ src/real.h
@@ -8,7 +8,22 @@
 
 #pragma once
 
+#if defined(FASTTEXT_Q16) || defined(FASTTEXT_Q32) || defined(FASTTEXT_F16) || defined(FASTTEXT_BF16)
+#include "qnum.hpp"
+#include "flex.hpp"
+#endif
+
 namespace fasttext {
 
+#if defined(FASTTEXT_Q16)
+typedef qnum::qspace_number_t<int16_t, 3, 0, true> real;
+#elif defined(FASTTEXT_Q32)
+typedef qnum::qspace_number_t<int32_t, 3, 0, true> real;
+#elif defined(FASTTEXT_F16)
+typedef flex::flexfloat<5,10> real;
+#elif defined(FASTTEXT_BF16)
+typedef flex::flexfloat<8,7> real;
+#else
 typedef float real;
+#endif
 }
